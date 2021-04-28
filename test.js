717f6c6c0c72974162ac55b035b243acd697f160
Andrew Jones <andrewjonesdev1@gmail.com>
2021-04-28 16:20:20 -0500

    initial commit

---
+++ test.js
@@ -0,0 +1,27 @@
+function firstNameFunc(){ 
+    let firstName = document.getElementById("myFirstName");
+    console.log(firstName.value);
+    if(firstName.value.length>0){
+        document.getElementById("firstNameError").textContent= "";
+        alert("valid");
+    }
+    else{
+        document.getElementById("firstNameError").textContent = "first Name cannot be empty";
+        alert("invalid");
+    }
+}
+function watctchesFunc(){
+    
+}
+function firstNameFunc(){ 
+    let firstName = document.getElementById("myFirstName");
+    console.log(firstName.value);
+    if(firstName.value.length>0){
+        document.getElementById("firstNameError").textContent= "";
+        alert("valid");
+    }
+    else{
+        document.getElementById("firstNameError").textContent = "first Name cannot be empty";
+        alert("invalid");
+    }
+}
